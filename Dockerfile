195a8da5178f87ffce6420cc022ece3e62d6fac9
Sven Grunewaldt <strayer@olle-orks.org>
2020-02-20 21:18:14 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.8 AS build
+
+WORKDIR /app
+
+RUN pip --no-cache-dir install poetry==1.0.3
+
+RUN poetry config virtualenvs.in-project true
+
+COPY pyproject.toml poetry.lock ./
+
+RUN poetry install --no-dev
+
+COPY main.py ./
+
+FROM python:3.8-slim AS runtime
+
+WORKDIR /app
+ENV PATH="/app/.venv/bin:$PATH"
+
+COPY --from=build /app/ ./
+
+CMD [ "python", "main.py" ]
+
---
+++ main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import sys
+import requests
+from pprint import pprint
+from influxdb import InfluxDBClient
+import time
+import os
+import logging
+from datetime import datetime, timezone
+
+HELIALUX_SPECTRUM_IP=os.getenv("HELIALUX_SPECTRUM_IP")
+MEASUREMENT = os.getenv("INFLUXDB_DB")
+TIMEZONE = "Z"
+LOGLEVEL = os.getenv("LOGLEVEL", "INFO")
+
+
+logging.basicConfig(level=LOGLEVEL.upper())
+
+
+def boolean_env_is_true(env):
+    return os.getenv(env, "false").lower() == "true"
+
+
+def main():
+    influxdb_client = InfluxDBClient(
+        host=os.getenv("INFLUXDB_HOST"),
+        port=int(os.getenv("INFLUXDB_PORT", "8086")),
+        username=os.getenv("INFLUXDB_USER"),
+        password=os.getenv("INFLUXDB_PASSWORD"),
+        database=os.getenv("INFLUXDB_DB"),
+        ssl=boolean_env_is_true("INFLUXDB_SSL"),
+        verify_ssl=not boolean_env_is_true("INFLUXDB_NO_VERIFY_SSL"),
+    )
+
+    while True:
+        logging.debug("Loading new data…")
+
+        r = requests.post(f"http://{HELIALUX_SPECTRUM_IP}/stat", data={"action": "10"})
+        r.raise_for_status()
+
+        data = r.json()
+        led_state = data["C"]["ch"]
+
+        logging.debug(
+            f"Received white: {led_state[0]}, blue: {led_state[1]}, green: {led_state[2]}, red: {led_state[3]}"
+        )
+
+        point = {
+            "time": datetime.utcnow().replace(tzinfo=timezone.utc).isoformat(),
+            "measurement": MEASUREMENT,
+            "tags": {"device": "helialux-spectrum"},
+            "fields": {
+                "white": led_state[0],
+                "blue": led_state[1],
+                "green": led_state[2],
+                "red": led_state[3],
+            }
+        }
+
+        influxdb_client.write_points([point])
+
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
